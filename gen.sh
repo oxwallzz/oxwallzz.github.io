d8cd7b8a57080a8840bb6a1d8df143b624a34869
orangexarot <elia.neri2005@gmail.com>
2025-04-06 23:35:57 +0200

    fix: better gen.sh

--- gen.sh
+++ gen.sh
@@ -6,15 +6,15 @@ echo "Generating low res images"
 echo "Done"
 
 echo "Making zip file..."
+before=$(du -h Wallpapers.zip | awk '{print $1}')
 rm Wallpapers.zip
 zip -r Wallpapers.zip Wallpapers/Computer Wallpapers/Phone
+after=$(du -h Wallpapers.zip | awk '{print $1}')
 echo "Done"
-cp index.html index_original.html
 
 php gen.php > index.html
 echo "Changed index.html"
+echo
 
-echo "Diff between original and updated index.html:"
-colordiff -u index_original.html index.html
-
-rm index_original.html
+echo "Wallpapers.zip  before: ${before} after: ${after}"
+git status -s
