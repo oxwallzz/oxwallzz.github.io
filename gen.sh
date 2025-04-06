ab51800880ed29762099db196e30a3c29a8819e3
orangexarot <elia.neri2005@gmail.com>
2025-04-06 23:20:25 +0200

    fix: bruh why was I starting a server

--- gen.sh
+++ gen.sh
@@ -3,24 +3,18 @@
 
 echo "Generating low res images"
 ./lowres.sh
+echo "Done"
 
 echo "Making zip file..."
 rm Wallpapers.zip
 zip -r Wallpapers.zip Wallpapers/Computer Wallpapers/Phone
-
+echo "Done"
 cp index.html index_original.html
 
-echo "Starting php server..."
-php -S localhost:8000 > /dev/null 2>&1 & sleep 1
-
-curl -s localhost:8000/gen.php > index.html
+php gen.php > index.html
 echo "Changed index.html"
 
 echo "Diff between original and updated index.html:"
 colordiff -u index_original.html index.html
-
-PID=$(pgrep -f "php -S localhost:8000")
-kill $PID
-echo "Killed php server"
 
 rm index_original.html
